821510fa80a0e0546643402eb7d95a3e966106d3
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-05-07 00:04:04 +0200

    Added Dummy events to events pages, listed out

--- frontend/src/Pages/Events.jsx
+++ frontend/src/Pages/Events.jsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+    {
+        id: 'event1',
+        title: 'Image Gallery Showing'
+    },
+    {
+        id: 'event',
+        title: 'Art Gallery Showing'
+    }
+];
+
 function Events() {
     return <>
     <h1>Events Page</h1>
+    <ul>
+        {
+            DUMMY_EVENTS.map(event => <li key={ event.id }>  
+                <Link to={ event.id }> { event.title } </Link>
+            </li>)
+        }
+    </ul>
     </>
 }
 
