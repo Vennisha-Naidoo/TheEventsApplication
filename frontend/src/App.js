7975da8011283937a4784d82a9ab139589c8a2a2
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-05-06 02:58:45 +0200

    Added Routing and Route definitions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './Pages/Home';
+import Events from './Pages/Events';
+import EventDetails from './Pages/EventDetails';
+import NewEvent from './Pages/NewEvent';
+import EditEvent from './Pages/EditEvent';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/events', element: <Events /> },
+  { path: '/events/:eventId', element: <EventDetails /> },
+  { path: '/events/new', element: <NewEvent /> },
+  { path: '/events/:eventId/edit', element: <EditEvent /> },
+  
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={ router } />;
 }
 
 export default App;
