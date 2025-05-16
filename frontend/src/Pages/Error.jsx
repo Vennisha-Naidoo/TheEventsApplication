8ff78016676ecbd1ac21fc12b3cebcd38dba053b
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-05-16 23:43:15 +0200

    Extracted Error data and Throwing Responses

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -1,8 +1,24 @@
+import { useRouteError } from "react-router-dom";
 import PageContent from "../components/PageContent";
 
 function ErrorPage() {
-    return <PageContent title="An error has occurred!">
-        <p>Something went wrong.</p>
+
+    const error = useRouteError();
+
+    let title = "An error has occurred!";
+    let message = "Something went wrong.";
+
+    if (error.status === 500) {
+        message = JSON.parse(error.data).message;
+    }
+
+    if (error.status === 404 ) {
+        title = "Not Found!";
+        message = "Could not find resource or page.";
+    }
+
+    return <PageContent title={ title }>
+        <p> { message } </p>
     </PageContent>
 
 }
